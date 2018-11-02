2f32b3020e08c1e151afb5060edde9a27675a256
ggil <sound315315@gmail.com>
2018-11-02 19:42:34 +0900

    소유도서등록 작업중

--- my_incbook/src/main/java/com/incbook/project/controller/OwnController.java
+++ my_incbook/src/main/java/com/incbook/project/controller/OwnController.java
@@ -4,13 +4,16 @@ import javax.inject.Inject;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.incbook.project.domain.BookVO;
 import com.incbook.project.domain.MemberVO;
 import com.incbook.project.domain.OwnVO;
+import com.incbook.project.domain.RealationVO;
+import com.incbook.project.domain.pagemaker.PageMaker;
+import com.incbook.project.domain.searchcriteria.SearchCriteria;
 import com.incbook.project.service.BookService;
 import com.incbook.project.service.MemberService;
 import com.incbook.project.service.OwnService;
@@ -40,8 +43,29 @@ public class OwnController {
 	
 	@RequestMapping(value = "/ownInsert", method = RequestMethod.GET)
 	public void ownInsertGET(Model model, OwnVO vo) throws Exception {
+		model.addAttribute("own", vo);
+	}
+
+	@RequestMapping(value = "/ownInsert", method = RequestMethod.POST)
+	public String ownInsertPOST(Model model, RealationVO rvo, OwnVO ovo) throws Exception {
+		ownService.ownInsert(rvo, ovo);
+		
+		return "redirect:/";
+	}
+	
+	@RequestMapping(value = "/searchBook", method = RequestMethod.GET)
+	public void searchBookPOST(Model model, RealationVO rvo, OwnVO ovo, @ModelAttribute("cri") SearchCriteria cri) throws Exception {
+		model.addAttribute("list", bookService.searchList(cri));
+		
+		PageMaker pageMaker = new PageMaker();
+		pageMaker.setCri(cri);
+		pageMaker.setTotalCount(bookService.searchListCount(cri));
+
+		model.addAttribute("pageMaker", pageMaker);
 		
 	}
+	
+	
 	
 	
 
--- my_incbook/src/main/java/com/incbook/project/persistence/OwnDAOImpl.java
+++ my_incbook/src/main/java/com/incbook/project/persistence/OwnDAOImpl.java
@@ -1,5 +1,7 @@
 package com.incbook.project.persistence;
 
+import java.util.Map;
+
 import javax.inject.Inject;
 
 import org.apache.ibatis.session.SqlSession;
@@ -18,5 +20,10 @@ public class OwnDAOImpl implements OwnDAO{
 	@Override
 	public OwnVO findOwnByID(OwnVO vo) throws Exception {
 		return sqlSession.selectOne(namespace+".findOwnByID", vo);
+	}
+
+	@Override
+	public void ownInsert(Map<String, Object> ownMap) {
+		sqlSession.insert(namespace + ".ownInsert", ownMap);
 	}
 }
